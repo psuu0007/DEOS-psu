35751a4466efa2eaf7bbc9c7d7193fc00a6d5d24
psuu0007 <psuu0007@naver.com>
2023-06-17 22:39:48 +0900

    Update projectRecoveryTest.java
    문제 일부 수정

--- projectRecoveryPsu/src/projectRecoveryPsu/projectRecoveryTest.java
+++ projectRecoveryPsu/src/projectRecoveryPsu/projectRecoveryTest.java
@@ -5,8 +5,8 @@ public class projectRecoveryTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("주어진 자료를 기반으로 복원하여");
-		System.out.println("개발 환경을 구축할 수 있다");
+		System.out.println("본인명을 풀네임 영어로 작성하시오(ex내용을 지우고 입력)");
+		System.out.println("ex: Hong KilDong");
 		
 		
 	}
